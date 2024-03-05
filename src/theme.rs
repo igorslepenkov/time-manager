7c8ed6d2ff3ce86cb08db4bc0a10391b50fbbc54
igorslepenkov <slepenkov.nii@yandex.by>
2024-03-05 22:24:20 +0300

    refactor(feat/ratatui/add_ui): remove useless code

--- src/theme.rs
+++ src/theme.rs
@@ -16,14 +16,5 @@ pub const THEME: Theme = Theme {
     },
 };
 
-const DARK_BLUE: Color = Color::Rgb(16, 24, 48);
-const LIGHT_BLUE: Color = Color::Rgb(64, 96, 192);
-const LIGHT_YELLOW: Color = Color::Rgb(192, 192, 96);
-const LIGHT_GREEN: Color = Color::Rgb(64, 192, 96);
-const LIGHT_RED: Color = Color::Rgb(192, 96, 96);
-const RED: Color = Color::Rgb(215, 0, 0);
 const BLACK: Color = Color::Rgb(8, 8, 8); // not really black, often #080808
 const DARK_GRAY: Color = Color::Rgb(68, 68, 68);
-const MID_GRAY: Color = Color::Rgb(128, 128, 128);
-const LIGHT_GRAY: Color = Color::Rgb(188, 188, 188);
-const WHITE: Color = Color::Rgb(238, 238, 238); // not really white, often #eeeeee
